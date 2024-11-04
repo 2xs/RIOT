94b53d031ee0825361532fda19558a590fab49f3
Damien Amara <damien.amara@univ-lille.fr>
2024-11-04 16:27:40 +0100

    examples/xipfs: refactor some code in the main function

--- examples/xipfs/main.c
+++ examples/xipfs/main.c
@@ -52,39 +52,39 @@ XIPFS_NEW_PARTITION(xipfs_1, "/dev/nvme0p0", 10);
  */
 XIPFS_NEW_PARTITION(xipfs_2, "/dev/nvme0p1",  15);
 
+/**
+ * @internal
+ *
+ * @brief Mount a partition, or if it is corrupted, format and
+ * remount it
+ *
+ * @param xipfs_mp A pointer to a memory region containing an
+ * xipfs mount point structure
+ */
+static void mount_or_format(xipfs_mount_t *xipfs_mp)
+{
+    if (vfs_mount(&xipfs_mp->vfs) < 0) {
+        printf("vfs_mount: \"%s\": file system has not been "
+            "initialized or is corrupted\n", xipfs_mp->vfs.mount_point);
+        printf("vfs_format: \"%s\": try initializing it\n",
+            xipfs_mp->vfs.mount_point);
+        vfs_format(&xipfs_mp->vfs);
+        printf("vfs_format: \"%s\": OK\n", xipfs_mp->vfs.mount_point);
+        if (vfs_mount(&xipfs_mp->vfs) < 0) {
+            printf("vfs_mount: \"%s\": file system is corrupted!\n",
+                xipfs_mp->vfs.mount_point);
+            PANIC();
+        }
+    }
+    printf("vfs_mount: \"%s\": OK\n", xipfs_mp->vfs.mount_point);
+}
+
 int main(void)
 {
     char line_buf[SHELL_DEFAULT_BUFSIZE];
 
-    if (vfs_mount(&xipfs_1.vfs) < 0) {
-        printf("vfs_mount: \"%s\": file system has not been "
-            "initialized or is corrupted\n", xipfs_1.vfs.mount_point);
-        printf("vfs_format: \"%s\": try initializing it\n",
-            xipfs_1.vfs.mount_point);
-        vfs_format(&xipfs_1.vfs);
-        printf("vfs_format: \"%s\": OK\n", xipfs_1.vfs.mount_point);
-        if (vfs_mount(&xipfs_1.vfs) < 0) {
-            printf("vfs_mount: \"%s\": file system is corrupted!\n",
-                xipfs_1.vfs.mount_point);
-            PANIC();
-        }
-    }
-    printf("vfs_mount: \"%s\": OK\n", xipfs_1.vfs.mount_point);
-
-    if (vfs_mount(&xipfs_2.vfs) < 0) {
-        printf("vfs_mount: \"%s\": file system has not been "
-            "initialized or is corrupted\n", xipfs_2.vfs.mount_point);
-        printf("vfs_format: \"%s\": try initializing it\n",
-            xipfs_2.vfs.mount_point);
-        vfs_format(&xipfs_2.vfs);
-        printf("vfs_format: \"%s\": OK\n", xipfs_2.vfs.mount_point);
-        if (vfs_mount(&xipfs_2.vfs) < 0) {
-            printf("vfs_mount: \"%s\": file system is corrupted!\n",
-                xipfs_2.vfs.mount_point);
-            PANIC();
-        }
-    }
-    printf("vfs_mount: \"%s\": OK\n", xipfs_2.vfs.mount_point);
+    mount_or_format(&xipfs_1);
+    mount_or_format(&xipfs_2);
 
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
